82ebf48121f8e4a2ded29f2bc83a202c9d060886
Ikechukwu Ugbo <74150930+iykeee@users.noreply.github.com>
2020-11-12 12:23:04 +0100

    spderCleanup.py

--- spyderCleanup.py
+++ spyderCleanup.py
@@ -4,35 +4,37 @@ from bs4 import BeautifulSoup
 #soup = BeautifulSoup("<p>Some<b>bad<i>HTML", features="html.parser")
 #print(soup.prettify())
 
-htmlHandle = None
+fileHandle = None
 
-while True:
-    if (htmlHandle == " "):
-        print(f"{htmlHandle} OK!")
-        break
+class cleanup:
+    def __init__ (self, fileHandle):
+        self.htmlHandle = fileHandle
 
-    else:
-        htmlHandle = input("Enter file name: ")
+    def scrape (self, htmlHandle):
+        while True:
+            if (type(htmlHandle) is not str):
+                print(f"{htmlHandle} is not a valid file name \n Kindly enter correct file name in .html format")
+                continue
+            else:
+                print(f"{htmlHandle} is OK!")
+                break
+        
+        htmlTree = open(htmlHandle)
+        print(htmlTree)
 
-    if (type(htmlHandle) is not str):
-        print(f"{htmlHandle} is not a valid file name \n Kindly enter correct file name in .html format")
-        continue
+        soup = BeautifulSoup(htmlTree, features="html.parser")
+        anchorTags = None
+        print(f"Retrieving all tags from {htmlHandle}\n Kindly wait... \n Loading...\n All Done!")
 
-    else:
-        break
+        #look for anchor tags in doc.
+        anchorTags = soup('a')
+        print(f"Retrieving all anchor tags from {htmlHandle}\n Kindly wait... \n Loading...{anchorTags}\n All Done!")
 
-htmlTree = open(htmlHandle)
-print(htmlTree)
+        for links in anchorTags:
+            print(links)
+        
+        return True
 
-soup = BeautifulSoup(htmlTree, features="html.parser")
-anchorTags = None
-Tags = soup
-print(f"Retrieving all tags from {htmlHandle}\n Kindly wait... \n Loading...\n All Done!")
 
-#look for anchor tags in doc.
-anchorTags = soup('a')
-print(f"Retrieving all anchor tags from {htmlHandle}\n Kindly wait... \n Loading...{anchorTags}\n All Done!")
 
-for links in anchorTags:
-    print(links)
 
