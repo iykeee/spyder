c84434d7ab2a4b072b6c783ff0898693fccce8cf
Ikechukwu Ugbo <74150930+iykeee@users.noreply.github.com>
2020-11-12 15:01:52 +0100

    spyderClean.py

--- spyder.py
+++ spyder.py
@@ -1,6 +1,5 @@
 from spyderCleanup import cleanup
 
+animePahe = cleanup("animePahe.html")
+animePahe.scrape("animePahe.html")
 
-paheScrape = cleanup("animePahe.html")
-
-paheScrape.scrape("animePahe.html")
