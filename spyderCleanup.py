6a03d9bd11655f6f94f7a22fa578b645b2722ed6
Ikechukwu Ugbo <74150930+iykeee@users.noreply.github.com>
2020-11-12 11:26:52 +0100

    INITIAL SPIDER CODE

---
+++ spyderCleanup.py
@@ -0,0 +1,38 @@
+import sqlite3
+import html
+from bs4 import BeautifulSoup
+#soup = BeautifulSoup("<p>Some<b>bad<i>HTML", features="html.parser")
+#print(soup.prettify())
+
+htmlHandle = None
+
+while True:
+    if (htmlHandle == " "):
+        print(f"{htmlHandle} OK!")
+        break
+
+    else:
+        htmlHandle = input("Enter file name: ")
+
+    if (type(htmlHandle) is not str):
+        print(f"{htmlHandle} is not a valid file name \n Kindly enter correct file name in .html format")
+        continue
+
+    else:
+        break
+
+htmlTree = open(htmlHandle)
+print(htmlTree)
+
+soup = BeautifulSoup(htmlTree, features="html.parser")
+anchorTags = None
+Tags = soup
+print(f"Retrieving all tags from {htmlHandle}\n Kindly wait... \n Loading...\n All Done!")
+
+#look for anchor tags in doc.
+anchorTags = soup('a')
+print(f"Retrieving all anchor tags from {htmlHandle}\n Kindly wait... \n Loading...{anchorTags}\n All Done!")
+
+for links in anchorTags:
+    print(links)
+
