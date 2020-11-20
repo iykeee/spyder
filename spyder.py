4525d31ebbe9243f5cfa72631cfb3d9cfc08a26e
Ikechukwu Ugbo <74150930+iykeee@users.noreply.github.com>
2020-11-20 14:17:48 +0100

    Spyder 1.0

--- spyder.py
+++ spyder.py
@@ -1,19 +1,21 @@
-from requests import req
-from spyderCleanup import cleanup
-import sys
+#import re
+#import sys
+#import webbrowser
+#from requests import req
+#from spyderCleanup import cleanup
 
+#Default template for the Spyder Application; 
+#Many a times the spidering of web includes the use of regular expression; However I, Iykee, the author of this Spyder application has given you full liberty to alter and carry out whatever algorithm you see fit.
+#This is yet to be at the pypi packages, you could just clone it from GitHub and manually install it using "pip install spyder"
+#spyder = req.htmlGenerator()
 
-#htmlHandle = req.htmlGenerator(req, "https://google.com")
-try:
-    spyder = cleanup.scrape(self=input("Name of website: "))
+#try:
+    #spyder = cleanup.scrape()
 
-except KeyboardInterrupt:
-    print(f"Error! url not valid")
-    sys.exit(1)
+#except KeyboardInterrupt:
+    #print(f"Program Interrupted by user! \n Terminating program")
+    #sys.exit(1)
 
-
-#animePahe = cleanup("animePahe.html")
-#animePahe.scrape("animePahe.html")
 
 
 
