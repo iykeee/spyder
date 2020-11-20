e2853ab5c8c16fd2703b927be02da7675731497f
Ikechukwu Ugbo <74150930+iykeee@users.noreply.github.com>
2020-11-20 14:28:06 +0100

    SPYDER 1.0

--- spyderCleanup.py
+++ spyderCleanup.py
@@ -1,38 +1,49 @@
 import sqlite3
 import html
 from bs4 import BeautifulSoup
-#soup = BeautifulSoup("<p>Some<b>bad<i>HTML", features="html.parser")
-#print(soup.prettify())
+from requests import req
+import webbrowser
 
-htmlHandle = None
+fileHandle = None
 
-while True:
-    if (htmlHandle == " "):
-        print(f"{htmlHandle} OK!")
-        break
+class cleanup(req):
 
-    else:
-        htmlHandle = input("Enter file name: ")
+    def __init__ (self, fileHandle):
+        self.htmlHandle = fileHandle
 
-    if (type(htmlHandle) is not str):
-        print(f"{htmlHandle} is not a valid file name \n Kindly enter correct file name in .html format")
-        continue
+    def scrape (self, htmlTree):
 
-    else:
-        break
+        conn = sqlite3.connect('cleanup.sqlite')
+        cur = conn.cursor()
 
-htmlTree = open(htmlHandle)
-print(htmlTree)
+        cur.execute('''DROP TABLE IF EXISTS Spyder''')
+        cur.execute('''CREATE TABLE IF NOT EXISTS Spyder (id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, links TEXT UNIQUE)''')
 
-soup = BeautifulSoup(htmlTree, features="html.parser")
-anchorTags = None
-Tags = soup
-print(f"Retrieving all tags from {htmlHandle}\n Kindly wait... \n Loading...\n All Done!")
+        soup = BeautifulSoup(htmlTree, features="html5lib")
+        print(f"Retrieving all tags from html handle\n Kindly wait... \n Loading...\n All Done!")
 
-#look for anchor tags in doc.
-anchorTags = soup('a')
-print(f"Retrieving all anchor tags from {htmlHandle}\n Kindly wait... \n Loading...{anchorTags}\n All Done!")
+        #look for anchor tags in doc.
+        
+        anchorTags = soup('a')
+        print(f"Retrieving all anchor tags from html handle\n Kindly wait... \n Loading...\n All Done!")
 
-for links in anchorTags:
-    print(links)
+        print("Generating links...")
+        num = 1
+        link = list()
 
+
+        for links in anchorTags:
+            links = links.get("href", None)
+            if ( links is None ) : continue
+            
+            #print(f"{num} Getting Link... {links}")
+            links = str(links)
+            cur.execute('INSERT OR IGNORE INTO Spyder (links) VALUES (? )', (links, ) )
+            conn.commit()
+            num = num + 1
+            link.append(links)
+            #webbrowser.open_new_tab(links)
+
+        
+        cur.close()
+        return link
